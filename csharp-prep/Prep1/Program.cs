1e93a0bb52d5380f0428875388f84b150ac4e6cf
vinceoooy <vince.ookaay@gmail.com>
2024-01-10 02:27:10 +0800

    prep 1 and 2 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+
+        Console.WriteLine(); // this creates a blank line
+
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.Write("What is your grade in percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+        // Stretch Challenge
+        int lastDigit = grade % 10;
+        string sign = "";
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            letter = "A";
+            sign = "";
+        }
+        else if (letter == "F" && sign != "")
+        {
+            letter = "F";
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed");
+        }
+        else 
+        {
+            Console.WriteLine("Let's do better next time");
+        }
+
     }
 }
+
